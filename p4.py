6c0fd188453855b63d076af36c757fe385d3e6b7
Anshika Soni <91690050+SoniAnshika@users.noreply.github.com>
2023-01-18 22:55:26 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+n=int(input("Enter the number of neurons in the hidden layer: "))
+
+inputs=[]
+num=int(input("Number of Inputs: "))
+print("Enter the inputs: ")
+for i in range(0,num):
+    x=int(input())
+    inputs.append(x)
+
+def percept(n,inputs):
+    Ohp=[]
+    c=0
+    wts=np.random.randn(n*num)
+    print("Weights of Hidden Layer: ",wts)
+    for i in range(0,n):
+        o=0
+        for j in range(0,num):
+            o=o+wts[c]*inputs[j]
+            c=c+1
+        Ohp.append(o)
+    print("Outputs of Hidden Layer: ",Ohp)
+    y=0
+    Wts=np.random.randn(n)
+    print("Weights of Output Layer: ",Wts)
+    for i in range(0,n):
+        y=y+Wts[i]*Ohp[i]
+
+    return y
+
+Y=percept(n,inputs)
+print("Output: ",Y)
