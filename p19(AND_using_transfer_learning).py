f5cb441ef05895c9d47be333e89acc3fe26cba59
Anshika Soni <91690050+SoniAnshika@users.noreply.github.com>
2023-01-20 23:14:13 +0530

    Add files via upload

---
+++ p19(AND_using_transfer_learning).py
@@ -0,0 +1,39 @@
+import numpy as np
+
+x = np.array([[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]])
+y = np.array([0,0,0,0,0,0,0,1])
+
+
+def pred(x,w):
+    return 1 if sum(x*w) > 2 else 0
+
+def learning(x,y,w):
+    print('weight ',w)
+    for k in range(1):
+        print('------iteration ',k+1)
+        for i,j in zip(x,y):
+            if j - pred(i,w) > 0:
+                w = w + 0.01* i
+            elif j - pred(i,w) < 0:
+                w = w - 0.01* i
+            else:
+                a=1
+    return w
+
+np.random.seed(1)
+w = [1.97434536, 0.01824359, 0.01182825]
+w = learning(x,y,w)
+
+#Testing on training data
+print('Test on training data.....')
+for i,j in zip(x,y):
+    print(pred(i,w)==j)
+
+correct,incorrect =0,0
+for i,j in zip(x,y):
+    if pred(i,w)==j:
+        correct += 1
+    else:
+        incorrect += 1
+accuracy =(1.0* correct)/len(x)
+print('accuracy = ',accuracy*100,'%')
